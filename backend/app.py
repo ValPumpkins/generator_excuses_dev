d682fc339b360cb4bd86a9c353f39cc1bccc9d92
ValentinePumpkins <quignon.val@gmail.com>
2024-04-17 10:21:35 +0200

    main files for the flask app

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,28 @@
+""" Main file for the Flask application """
+
 from flask import Flask, g
+from flask_cors import CORS
+import sqlite3
 
 
 app = Flask(__name__)
+CORS(app)
 app.config['DATABASE'] = 'db/excuses.db'
 
-# Fonction pour ouvrir une connexion SQLite
+# Function to get a SQLite connection
 def get_db():
+    """ Open a new database connection if there is none yet """
     if 'db' not in g:
         g.db = sqlite3.connect(app.config['DATABASE'])
         g.db.row_factory = sqlite3.Row
+
     return g.db
 
 
+# Function to close the SQLite connection
 @app.teardown_appcontext
 def close_db(error):
+    """ Close the database at the end of the request """
     db = g.pop('db', None)
     if db is not None:
         db.close()
