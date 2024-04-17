77861474698ce864a23743105371c8eb128fa168
ValentinePumpkins <quignon.val@gmail.com>
2024-04-17 10:21:57 +0200

    all routes for api (list all / create / random)

--- backend/routes.py
+++ backend/routes.py
@@ -1,27 +1,64 @@
-from flask import jsonify, g
-from app import app
-import sqlite3
+""" All api routes are defined here """
+
+from flask import jsonify, request
+from app import app, get_db
+from utils.random_excuses import get_random_excuse
 
 
-# Fonction pour obtenir une connexion SQLite
-def get_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-# Route API pour récupérer toutes les excuses
-@app.route('/api/excuses')
+# Retrieve all excuses from the db
+@app.route('/api/excuses', methods=['GET'])
 def all_excuses():
+    """ API route to get the list of all excuses in db """
     db = get_db()
     cursor = db.cursor()
     cursor.execute('SELECT * FROM excuses')
     excuses = cursor.fetchall()
-    # Crée une liste de dictionnaires pour chaque excuse avec les noms de colonnes comme clés
+
+    # Create a list of dictionaries with the data
     excuses_list = [
         {
             'id': row[0],
             'http_code': row[1],
             'tag': row[2],
-            'message': row[3]}
+            'message': row[3]
+        }
         for row in excuses
     ]
 
-    return jsonify(excuses=excuses_list)
+    return jsonify(excuses_list)
+
+
+# Create a new excuse
+@app.route('/api/create', methods=['POST'])
+def create_excuse():
+    """ API route to add a new excuse to the db """
+    data = request.json
+    http_code = data.get('http_code')
+    tag = data.get('tag')
+    message = data.get('message')
+
+    # Check if all datas are present
+    if http_code is None or tag is None or message is None:
+        return jsonify({'Error': 'Missing datas'}), 400
+
+    db = get_db()
+    cursor = db.cursor()
+
+    # Insert the new excuse in the db
+    cursor.execute('INSERT INTO excuses (http_code, tag, message) VALUES (?, ?, ?)',
+                    (http_code, tag, message))
+    db.commit()
+
+    return jsonify({'message': 'Success !'}), 201
+
+# Get a random excuse
+@app.route('/api/random', methods=['GET'])
+def random_excuse():
+    """ API route to get a random excuse """
+    excuse = get_random_excuse()
+    
+    if excuse:
+        return jsonify(excuse=excuse), 200
+    else:
+        return jsonify({'error': 'No excuses available'}), 404
+
