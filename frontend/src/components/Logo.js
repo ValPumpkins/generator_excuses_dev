cf2798c5003d2464d65e338fb4c6bc1c95464361
ValentinePumpkins <quignon.val@gmail.com>
2024-04-18 17:13:20 +0200

    add logo + link to github

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+	return (
+    <div className="val-container">
+      <a href="https://github.com/ValPumpkins" target='blank'>
+        <img src="/favicon.ico" alt="brocoli icon" />
+      </a>
+    </div>
+  );
+};
+
+export default Logo;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import ExcuseGenerator from '../components/ExcuseGenerator';
+import Logo from '../components/Logo';
 
 
 const Home = () => {
   return (
     <div>
+      <Logo />
       <ExcuseGenerator />
     </div>
   );
