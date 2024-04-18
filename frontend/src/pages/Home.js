f95ae4bbc73c42c53366fbd9cf9c6fcb7bd03021
ValentinePumpkins <quignon.val@gmail.com>
2024-04-18 12:16:36 +0200

    rm create btn

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,12 @@
 
 import React from 'react';
 import ExcuseGenerator from '../components/ExcuseGenerator';
-import CreateExcuses from '../modals/CreateExcuses';
+
 
 const Home = () => {
   return (
     <div>
       <ExcuseGenerator />
-      <CreateExcuses />
     </div>
   );
 };
